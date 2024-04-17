118c096f019af928a3cd28ec0b2f671d77639a0d
J. Nick Koston <nick@koston.org>
2024-04-17 12:06:10 -0500

    feat: init version
    port code from aiohttp-zlib-ng and update for isal

--- src/aiohttp_isal/__init__.py
+++ src/aiohttp_isal/__init__.py
@@ -1,1 +1,35 @@
 __version__ = "0.0.0"
+
+import importlib
+import zlib as zlib_original
+
+import aiohttp
+from isal import isal_zlib as best_zlib
+
+TARGETS = (
+    "compression_utils",
+    "http_writer",
+    "http_websocket",
+    "http_writer",
+    "http_parser",
+    "multipart",
+    "web_response",
+)
+
+
+def enable_isal() -> None:
+    """Enable isal."""
+    for location in TARGETS:
+        try:
+            importlib.import_module(f"aiohttp.{location}")
+        except ImportError:
+            continue
+        if module := getattr(aiohttp, location, None):
+            module.zlib = best_zlib
+
+
+def disable_isal() -> None:
+    """Disable isal and restore the original zlib."""
+    for location in TARGETS:
+        if module := getattr(aiohttp, location, None):
+            module.zlib = zlib_original
