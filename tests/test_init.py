5b7b6061e3aafa625933d2b3865608c578f80fb8
J. Nick Koston <nick@koston.org>
2024-05-03 12:54:02 -0500

    feat: fallback to zlib when isal cannot be imported

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,4 +1,5 @@
 import zlib as zlib_original
+from unittest.mock import patch
 
 import aiohttp.http_websocket
 from isal import isal_zlib as expected_zlib
@@ -16,3 +17,12 @@ def test_enable_disable():
     enable_isal()
     assert aiohttp.http_websocket.zlib is expected_zlib
     disable_isal()
+
+
+def test_isal_not_available():
+    """Test isal not available."""
+    with patch("aiohttp_isal.ISAL_AVAILABLE", False):
+        enable_isal()
+        assert aiohttp.http_websocket.zlib is zlib_original
+        disable_isal()
+        assert aiohttp.http_websocket.zlib is zlib_original
